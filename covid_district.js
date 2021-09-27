1b9f61111626989bf2e3d5b3fc567d3826a0d266
ushapaswan <82705683+ushapaswan@users.noreply.github.com>
2021-09-27 20:16:19 +0530

    Update covid_district.js

--- covid_district.js
+++ covid_district.js
@@ -12,9 +12,45 @@ $(document).ready(function () {
             Object.entries(response).map(([keys,value]) => {
                 // console.log("usha",keys,value)
                 Object.entries(value.districts).map(([k,b])=>{
-                    // console.log("s",b["total"]["confirmed"])
+                    console.log("s",b)
                     var trHtml="";
-                    trHtml += "<tr><td>" + k + "</td><td>" + b["total"]["confirmed"] + "</td><td>" + b["total"]["deceased"] + "</td><td>" + b["total"]["vaccinated1"] + "</td><td>" + b["total"]["vaccinated2"] + "</td></tr>"
+                    trHtml += "<tr><td>" + k + "</td><td>" 
+                    if (b["total"]["confirmed"]===undefined){
+                        trHtml += "Not available" + "</td><td>" 
+                    }
+                    else{
+                        trHtml +=  b["total"]["confirmed"] + "</td><td>" 
+                    }
+                    
+                    if (b["total"]["deceased"]===undefined){
+                        trHtml +=  "Not available" + "</td><td>" 
+                    }
+                    else{
+                        trHtml +=   b["total"]["deceased"] + "</td><td>" 
+                    }
+
+                    if (b["total"]["recoverd"]===undefined){
+                        trHtml += "Not available" + "</td><td>" 
+                    }
+                    else{
+                        trHtml += b["total"]["recoverd"] + "</td><td>" 
+                    }
+                    
+                    if (b["total"]["vaccinated1"]===undefined){
+                        trHtml += "Not available" + "</td><td>" 
+                    }
+                    else{
+                        trHtml += b["total"]["vaccinated1"] + "</td><td>" 
+                    }
+                     
+                    if (b["total"]["vaccinated2"]===undefined){
+                        trHtml += "Not available" + "</td><td>" 
+                    }
+                    else{
+                        trHtml += b["total"]["vaccinated2"] + "</td><td>" 
+                    }
+                    // trHtml += "<tr><td>" + k + "</td><td>" + b["total"]["confirmed"] + "</td><td>" + b["total"]["deceased"] + "</td><td>" + b["total"]["vaccinated1"] + "</td><td>" + b["total"]["vaccinated2"] + "</td></tr>"
+                     
 
                     $("#records_table").append(trHtml);  
                         }) 
@@ -31,4 +67,4 @@ $(document).ready(function () {
 
 
 
-});
+}); 
